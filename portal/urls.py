b8658f31c8a9b6b2fc3ca9573f4127013d7af5e2
Nick Marden <nick@marden.org>
2020-05-27 01:18:39 -0400

    HL PoC template starter

--- portal/urls.py
+++ portal/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('hlpoc/', include('hlpoc.urls')),
     path('admin/', admin.site.urls),
-    path('healthz', include('health_check.urls'))
+    path('healthz', include('health_check.urls')),
+    path('', RedirectView.as_view(url='hlpoc/', permanent=False))
 ]
