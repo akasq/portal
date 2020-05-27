198ac63a576f7c35dfc35d80082bdd7958790700
Nick Marden <nick@marden.org>
2020-05-26 23:08:38 -0400

    Add health check endpoints

--- portal/urls.py
+++ portal/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('hlpoc/', include('hlpoc.urls')),
     path('admin/', admin.site.urls),
+    path('healthz', include('health_check.urls'))
 ]
